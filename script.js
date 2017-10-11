bc1df3f31263f9cac595ec6aed62123f9d750195
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2017-10-11 03:54:00 +0200

    commit1

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 var movies = [
-		{
+		{	
+			id: 1,
 			title: 'Harry Potter',
 			desc: 'film o czarodzieju',
 			img: 'https://cdn.pixabay.com/photo/2017/10/06/16/50/bird-2823763__340.jpg'
 		},
-		{
+		{	
+			id: 2,
 			title: 'Król Lew',
 			desc: 'Film o królu sawanny',
 			img: 'https://cdn.pixabay.com/photo/2017/10/06/16/50/pine-cones-2823765__340.jpg'
 		},
-		{
+		{	
+			id: 3,
 			title: 'The Shawshank Redemption',
 			desc: 'Adaptacja opowiadania Stephena Kinga. Niesłusznie skazany na dożywocie bankier, stara się przetrwać w brutalnym, więziennym świecie.',
 			img: 'https://cdn.pixabay.com/photo/2017/10/02/17/49/mushroom-2809620__340.jpg'
 		},
-		{
+		{	
+			id: 4,
 			title: 'Intouchables',
 			desc: 'Sparaliżowany milioner zatrudnia do opieki młodego chłopaka z przedmieścia, który właśnie wyszedł z więzienia.',
 			img: 'https://cdn.pixabay.com/photo/2017/10/05/22/16/katz-2821316__340.jpg'
@@ -22,7 +26,7 @@ var movies = [
 		];	
 
 var moviesElements = movies.map(function(movie) {
-		return 	React.createElement('li', {},
+		return 	React.createElement('li', {key: movie.id},
 					React.createElement('h2', {}, movie.title),
 					React.createElement('p', {}, movie.desc),
 					React.createElement('img', {src: movie.img})
